902a2468ee6e77e674e41c25cdfa996b4179eda8
Alexis <alexis.parra.pallares@gmail.com>
2021-03-05 09:12:49 +0100

    exclude URI

--- DB/connection.js
+++ DB/connection.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-// const URI = "mongodb+srv://admin:TSbLnXczZYnsC0D6@cluster0.ytt0s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 const connectDB = async() => {
     await mongoose.connect(process.env.MONGODB_URL, { useUnifiedTopology: true, useNewUrlParser: true })
